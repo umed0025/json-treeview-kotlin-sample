952e081a9160b541e95c57c6e691ccb0eb97cba6
umed0025 <umed0025@gmail.com>
2022-07-07 13:05:07 +0900

    * ロード処理追加 * 初期データをロードしないよう修正

--- src/main/kotlin/jp/dkosilver/json/fx/SampleApplication.kt
+++ src/main/kotlin/jp/dkosilver/json/fx/SampleApplication.kt
@@ -1,7 +1,5 @@
 package jp.dkosilver.json.fx
 
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.json.JsonMapper
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
@@ -9,13 +7,7 @@ import javafx.stage.Stage
 
 class SampleApplication : Application() {
     override fun start(stage: Stage) {
-        val url = TreeViewController::class.java.getResource("/data.json")
-        TreeViewController.log.info("url:{}", url)
-        val jsonNode: JsonNode = JsonMapper().readTree(url)
-
         val fxmlLoader = FXMLLoader(SampleApplication::class.java.getResource("tree-view.fxml"))
-        val controller = TreeViewController(jsonNode)
-        fxmlLoader.setController(controller)
         val scene = Scene(fxmlLoader.load())
         stage.title = "TreeView"
         stage.scene = scene
--- src/main/kotlin/jp/dkosilver/json/fx/TreeViewController.kt
+++ src/main/kotlin/jp/dkosilver/json/fx/TreeViewController.kt
@@ -1,17 +1,22 @@
 package jp.dkosilver.json.fx
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.json.JsonMapper
 import javafx.beans.property.ReadOnlyStringWrapper
 import javafx.collections.ObservableList
 import javafx.fxml.FXML
 import javafx.scene.control.TreeItem
 import javafx.scene.control.TreeTableColumn
 import javafx.scene.control.TreeTableView
+import javafx.scene.control.cell.TextFieldTreeTableCell
+import javafx.scene.layout.AnchorPane
+import javafx.stage.FileChooser
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import java.io.File
 
 
-class TreeViewController(private val jsonNode: JsonNode) {
+class TreeViewController {
     companion object {
         @JvmStatic
         val log: Logger = LogManager.getLogger(TreeViewController::class.java)
@@ -20,18 +25,52 @@ class TreeViewController(private val jsonNode: JsonNode) {
     @FXML
     lateinit var treeTableView: TreeTableView<JsonData>
 
+    @FXML
+    lateinit var rootPane: AnchorPane
+
+    lateinit var jsonNode: JsonNode
+
     @Suppress("unused")
     fun initialize() {
         log.trace("start initialize")
-        dataInitialize()
+        log.trace("end initialize")
     }
 
-    private fun dataInitialize() {
+    @FXML
+    fun handleLoadButtonAction() {
+        // FIXME 汎用にする場合は不要
+        val fileChooser = FileChooser()
+        val url = TreeViewController::class.java.getResource("/data.json")
+        if (url != null) {
+            fileChooser.initialDirectory = File(url.toURI()).parentFile
+        }
+        fileChooser.title = "load json data"
+        fileChooser.initialFileName = "data.json"
+        val selectedFile = fileChooser.showOpenDialog(rootPane.scene.window)
+        if (selectedFile != null) {
+            load(selectedFile)
+        }
+    }
 
+    @FXML
+    fun handleSaveButtonAction() {
+        // TODO 実装が必要
+    }
+
+    private fun load(selectedFile: File) {
+        log.info("selectedFile absolutePath:{}", selectedFile.absolutePath)
+        jsonNode = JsonMapper().readTree(selectedFile)
+
+        // 編集可能に設定。（実際の編集可能設定については個別に設定が必要）
+        treeTableView.isEditable = true
+
+        // treeView用データ設定
         val viewRoot = TreeItem(JsonData("/", "root", ""))
         for (field in jsonNode.fields()) {
             createTreeItem(viewRoot.children, "/", field.key, field.value)
         }
+
+        // 名前列 表示設定
         val nameColumn = TreeTableColumn<JsonData, String>("name")
         nameColumn.prefWidth = 150.0
         nameColumn.setCellValueFactory { param: TreeTableColumn.CellDataFeatures<JsonData, String?> ->
@@ -39,6 +78,7 @@ class TreeViewController(private val jsonNode: JsonNode) {
                 param.value.value.name
             )
         }
+        // 値列 表示設定
         val valueColumn = TreeTableColumn<JsonData, String>("value")
         valueColumn.prefWidth = 150.0
         valueColumn.setCellValueFactory { param: TreeTableColumn.CellDataFeatures<JsonData, String?> ->
@@ -46,6 +86,10 @@ class TreeViewController(private val jsonNode: JsonNode) {
                 param.value.value.value
             )
         }
+        // 値列編集設定
+        valueColumn.cellFactory = TextFieldTreeTableCell.forTreeTableColumn()
+
+        // パス列表示設定
         val pathColumn = TreeTableColumn<JsonData, String>("path")
         pathColumn.prefWidth = 150.0
         pathColumn.setCellValueFactory { param: TreeTableColumn.CellDataFeatures<JsonData, String?> ->
@@ -53,7 +97,9 @@ class TreeViewController(private val jsonNode: JsonNode) {
                 param.value.value.path
             )
         }
+        // treeViewへデータ設定
         treeTableView.root = viewRoot
+        // treeView表示設定
         treeTableView.columns.setAll(nameColumn, valueColumn, pathColumn)
     }
 
@@ -94,6 +140,8 @@ class TreeViewController(private val jsonNode: JsonNode) {
     ) {
         createTreeItem(children, parentPath, index.toString(), node)
     }
+
+
 }
 
 
